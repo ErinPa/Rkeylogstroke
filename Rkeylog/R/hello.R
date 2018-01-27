847f79b8740ca78a6b457e08879823be615a32b3
Nicolas Ballier <nballier@free.fr>
2018-01-27 14:35:37 +0100

    first commit
    First commit of the prototype

---
+++ Rkeylog/R/hello.R
@@ -0,0 +1,15 @@
+# This is a draft for a keylog package
+# which includes an example function named 'hello'
+# which prints 'Hello, world!'.
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+
